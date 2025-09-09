5098116ed4a50f71a58af030dc08464e84bed0e8
lalit holkar <lalitholkar@gmail.com>
2025-09-09 14:25:28 +0530

    Add project files.

---
+++ VideoSplitter.Designer.cs
@@ -0,0 +1,258 @@
+﻿using System;
+
+namespace VideoCutter
+{
+    partial class VideoSplitter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtVideoName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ddlSplitTime = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.lblstatus);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtVideoName);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.ddlSplitTime);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(702, 557);
+            this.panel1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(92, 512);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 15;
+            this.progressBar1.Visible = false;
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(173, 538);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(0, 13);
+            this.lblstatus.TabIndex = 14;
+            this.lblstatus.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(321, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Split Video";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.split_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(481, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "path";
+            this.label7.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(349, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Download Path";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(485, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "path";
+            this.label6.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Upload Video";
+            // 
+            // txtVideoName
+            // 
+            this.txtVideoName.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtVideoName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtVideoName.Location = new System.Drawing.Point(349, 165);
+            this.txtVideoName.Name = "txtVideoName";
+            this.txtVideoName.Size = new System.Drawing.Size(152, 20);
+            this.txtVideoName.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Video Name";
+            // 
+            // ddlSplitTime
+            // 
+            this.ddlSplitTime.FormattingEnabled = true;
+            this.ddlSplitTime.Location = new System.Drawing.Point(349, 116);
+            this.ddlSplitTime.Name = "ddlSplitTime";
+            this.ddlSplitTime.Size = new System.Drawing.Size(152, 21);
+            this.ddlSplitTime.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select the Time to Split(In seconds) ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Location = new System.Drawing.Point(226, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Video Splitter";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(92, 363);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(192, 23);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // VideoSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 572);
+            this.Controls.Add(this.panel1);
+            this.Name = "VideoSplitter";
+            this.Text = "Video Splitter";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ddlSplitTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtVideoName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblstatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
---
+++ VideoSplitter.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoCutter
+{
+    public partial class VideoSplitter : Form
+    {
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPath;
+        public VideoSplitter()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindddlSplitTime();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Video Files|*.mp4;*.avi;*.mov;*.mkv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                label6.Visible = false;
+                label6.Text = ofd.FileName;
+                button1.Text = "Uploaded";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select folder to save split video files";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                label7.Visible = false;
+                label7.Text = fbd.SelectedPath;
+                button2.Text = "Selected";
+            }
+        }
+
+        private async void split_Click(object sender, EventArgs e)
+        {
+
+            string videoPath = label6.Text;
+            string outputDir = label7.Text;
+
+            if (!File.Exists(videoPath))
+            {
+                MessageBox.Show("Please select a valid video file.");
+                return;
+            }
+
+            ComboBoxItem selectedItem = ddlSplitTime.SelectedItem as ComboBoxItem;
+            int selectedTime = selectedItem?.Value ?? 0;
+            if (!int.TryParse(selectedTime.ToString(), out int segmentTime) || segmentTime <= 0)
+            {
+                MessageBox.Show("Enter a valid number of seconds.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(outputDir) || !Directory.Exists(outputDir))
+            {
+                MessageBox.Show("Please select an output folder.");
+                return;
+            }
+            EnableDisableControls(false);
+            string rawName = txtVideoName.Text?.Trim();
+            string videoName = string.IsNullOrWhiteSpace(rawName) ? "part_%03d.mp4" : $"{rawName}_%03d.mp4";
+            string outputPattern = Path.Combine(outputDir, videoName);
+            string arguments = $"-i \"{videoPath}\" -map 0 -c:v libx264 -preset fast -crf 23 -c:a aac -movflags +faststart -f segment -segment_time {segmentTime} -reset_timestamps 1 \"{outputPattern}\"";
+
+
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    ProcessStartInfo psi = new ProcessStartInfo
+                    {
+                        FileName = "ffmpeg",
+                        Arguments = arguments,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+
+                    using (Process proc = new Process { StartInfo = psi })
+                    {
+                        proc.Start();
+                        string errorOutput = proc.StandardError.ReadToEnd();
+                        proc.WaitForExit();
+                    }
+                });
+
+                button3.Text = "Split";
+                lblstatus.Text = "✅ Split completed!";
+                lblstatus.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                EnableDisableControls(true);
+            }
+        }
+
+        private void BindddlSplitTime()
+        {
+            ddlSplitTime.Items.Clear();
+            ddlSplitTime.Items.Add(new ComboBoxItem("30 seconds", 30));
+            ddlSplitTime.Items.Add(new ComboBoxItem("1 minute", 60));
+            ddlSplitTime.Items.Add(new ComboBoxItem("2 minutes", 120));
+            ddlSplitTime.Items.Add(new ComboBoxItem("3 minutes", 180));
+            ddlSplitTime.Items.Add(new ComboBoxItem("4 minutes", 240));
+            ddlSplitTime.Items.Add(new ComboBoxItem("5 minutes", 300));
+            ddlSplitTime.Items.Add(new ComboBoxItem("10 minutes", 600));
+            ddlSplitTime.SelectedIndex = 0;
+        }
+        private void EnableDisableControls(bool status)
+        {
+            if (status == false)
+            {
+                progressBar1.Location = new System.Drawing.Point(
+    (this.ClientSize.Width - progressBar1.Width) / 2,
+    (this.ClientSize.Height - progressBar1.Height) / 2
+);
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                progressBar1.MarqueeAnimationSpeed = 30;
+                progressBar1.BringToFront();
+            }
+            progressBar1.Visible = status == false ? true : false;
+            button1.Enabled = status;
+            button2.Enabled = status;
+            button3.Enabled = status;
+            ddlSplitTime.Enabled = status;
+            txtVideoName.Enabled = status;
+            btnClear.Enabled = status;
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+
+
+    public class ComboBoxItem
+    {
+        public string Text { get; set; }
+        public int Value { get; set; }
+        public ComboBoxItem(string text, int value)
+        {
+            Text = text;
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return Text; // Displayed in dropdown
+        }
+    }
+}
